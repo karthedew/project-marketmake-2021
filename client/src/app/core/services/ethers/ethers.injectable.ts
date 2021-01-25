84d61046f32780313954490e5e68d801f870ecf9
karthedew <karthedew@gmail.com>
2021-01-24 23:00:36 -0800

    Added a few changes to web3 injectables, added configuration parameters, deleted a few moved files.

--- client/src/app/core/services/ethers/ethers.injectable.ts
+++ client/src/app/core/services/ethers/ethers.injectable.ts
@@ -1,8 +1,9 @@
 import { ElementSchemaRegistry } from "@angular/compiler";
 import { InjectionToken } from "@angular/core";
 import { ethers, getDefaultProvider, providers } from "ethers";
-
+// import detectEthereumProvider from "@metamask/detect-provider";
 import { EtherWindow } from "../../interfaces/window.interface";
+import Web3 from "web3";
 // import {  } from "module";
 
 /*
@@ -10,31 +11,41 @@ import { EtherWindow } from "../../interfaces/window.interface";
 */
 
 // const CONTRACT_ADDRESS = 
-var win: EtherWindow
+// var window: EtherWindow
 
+/**
+ * This connects to the Localhost Provider
+ */
 const RpcProvider = new InjectionToken<providers.JsonRpcProvider>('HardHat Ethereum RPC Provider', {
     providedIn: 'root',
     factory: () => new providers.JsonRpcProvider('http://localhost:8545')
 })
 
-const MetaMaskProvider = new InjectionToken<any>('MetaMask Connected', {
+/**
+ * This connects to the current MetaMask provider
+ */
+const MetaMaskProvider = new InjectionToken<providers.Web3Provider>('MetaMask Connected', {
     providedIn: 'root',
-    factory: () => new ethers.providers.Web3Provider(win.ethereum)
+    factory: () => {
+        const ethersProvider = new ethers.providers.Web3Provider((window as any).ethereum);
+        return ethersProvider
+    }
 })
 
 
-// await window.ethereum.enable()
-// const provider = new ethers.providers.Web3Provider(window.ethereum);
-// const signer = provider.getSigner();
-// const RopstenProvider = new InjectionToken<any>('')
-
-// const provider = new ethers.providers.JsonRpcProvider('http://localhost:8545')
 /**
- * for MetaMask
- * const provider = new ethers.providers.Web3Provider(window.ethereum);
- * 
- * for connection to main-net
- * const provider = new ethers.providers.getDefaultProvider();
+ * This connects to the current window provider with Web3.js.
  */
+const WEB3 = new InjectionToken<Web3>('web3', {
+    providedIn: 'root',
+    factory: () => {
+        try {
+            const provider = ('ethereum' in window) ? window['ethereum'] : Web3.givenProvider;
+            return new Web3(provider);
+        } catch (err) {
+            throw new Error('Non-Ethereum browser detected. You should consider trying Mist or MetaMask!');
+        }
+    }
+});
 
- export { RpcProvider }
+ export { RpcProvider, MetaMaskProvider }
