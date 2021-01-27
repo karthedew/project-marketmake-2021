dad24fe0233a8b6246e083821b6bd2bc72cfdd14
Kand-h <hshuttleworth0@gmail.com>
2021-01-27 13:58:59 +0000

    Add basic metamask popup

--- client/src/app/core/services/ethers/appsigner.service.ts
+++ client/src/app/core/services/ethers/appsigner.service.ts
@@ -10,7 +10,7 @@ export class AppSigner extends Signer {
 
     private wallet: Wallet;
 
-    private wallet_key: string = process.env.PRIVATE_KEY;
+    private wallet_key: string = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
 
     constructor(
         @Inject(RpcProvider) public rpcProvider: providers.JsonRpcProvider
