5e1c409bdc0a14f074bf434eed87cfcf17a54ba1
karthedew <karthedew@gmail.com>
2021-01-27 11:38:38 -0800

    Ethers Contract for chainlink test contract

---
+++ client/src/app/core/services/contracts/chainlinkContract.services.ts
@@ -0,0 +1,27 @@
+
+import { Inject, Injectable } from '@angular/core';
+import { Contract, ethers, providers } from 'ethers';
+
+// // --- THE CONTRACT DATA ---
+import * as PriceConsumerJson from '../../../../contracts/PriceConsumerV3.sol/PriceConsumerV3.json'
+import { MetaMaskProvider } from '../ethers/ethers.injectable';
+
+
+/**
+ * This is an Angular Injectable class for connecting the client to the 
+ * TokenContract through the HardHat RPC localhost network. 
+ * 
+ *    NOTE: This is just an example and should be the methodology used
+ *          for connecting to and interacting with the smart contract.
+ */
+@Injectable({ providedIn: 'root' })
+export class ChainLinkContract extends Contract {
+
+    constructor(
+        @Inject(MetaMaskProvider) metaMaskProvider: providers.Web3Provider
+    ) {
+        let priceConsumerAddress = "0xCdd5083844Bed450fb7353e5606B85EFc790D03f";
+        let signer = metaMaskProvider.getSigner()
+        super(priceConsumerAddress, PriceConsumerJson.abi, signer);
+  }
+}
