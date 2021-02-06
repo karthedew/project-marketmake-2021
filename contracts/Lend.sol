2272049a09571e4ceaf31064caa15c0ca3426b4a
Kand-h <hshuttleworth0@gmail.com>
2021-02-06 19:58:16 +0000

    Add events

--- contracts/Lend.sol
+++ contracts/Lend.sol
@@ -21,6 +21,14 @@ contract Lend {
     mapping(address => UserStruct) userStructs;
     address payable giveAddress;
 
+    event Deposit(uint _totalDeposit, uint _interest);
+    event DepositAave(uint _aTokenBalance); 
+    event Withdraw(address _beneficiary, uint _amount, bool _deleted); 
+    event Give(uint _amount);
+    event AdjustGivingRate(address _user, uint _percentInterest);
+    event AddressUpdated(address _admin, address _giveAddress);
+    event AdminUpdated(address _oldAdmin, address _newAdmin);
+
     receive() external payable {}
 
     // For MAINNET + LOCAL, needs updating for KOVAN
@@ -44,11 +52,13 @@ contract Lend {
         userStructs[msg.sender].givePercent = _interest;
         userStructs[msg.sender].index = userIndex.length-1;
 
+        emit Deposit(userStructs[msg.sender].totalDeposited, _interest);
+
         depositAave(msg.sender, msg.value);
     }
 
     // Check if a user exists
-    function checkUserExistence(address _userAddress) internal view returns(bool) {
+    function checkUserExistence(address _userAddress) public view returns(bool) {
         uint _index = userStructs[_userAddress].index;
         address _indexed_address = userIndex[_index];
         if (_userAddress == _indexed_address) {
@@ -63,14 +73,15 @@ contract Lend {
         gateway.depositETH{value: _amount}(address(this), 0);
         userStructs[_spender].aTokenBalance += userStructs[_spender].aTokenBalance + _amount;
         lastATokenBalance = aWETH.balanceOf(address(this));
+        emit DepositAave(userStructs[_spender].aTokenBalance);
     }
 
     // Scales up aToken balances for all users to account for accrued interest
     function scaleBalances() private {
         uint _aTokenBalance = aWETH.balanceOf(address(this));
-        uint scalingFactor = (_aTokenBalance * (10 ** 2))/lastATokenBalance;
+        uint scalingFactor = (_aTokenBalance * 100)/lastATokenBalance;
         for (uint i=0; i < userIndex.length; i++) {
-            userStructs[userIndex[0]].aTokenBalance = userStructs[userIndex[0]].aTokenBalance * scalingFactor;
+            userStructs[userIndex[0]].aTokenBalance = userStructs[userIndex[0]].aTokenBalance * (scalingFactor/100);
         }
     }
 
@@ -85,6 +96,7 @@ contract Lend {
             gateway.withdrawETH(_amount, msg.sender);
         }
         lastATokenBalance = aWETH.balanceOf(address(this));
+        emit Withdraw(msg.sender, _amount, _all);
     }
 
     // Remove a user from userIndex and reorder
@@ -107,12 +119,16 @@ contract Lend {
         }
         aWETH.transfer(giveAddress, _giveAmount);
         lastATokenBalance = aWETH.balanceOf(address(this));
+
+        emit Give(_giveAmount);
     }
 
     // Adjust the user's give percentage
-    function adjust_giving_rate(uint _percentInterest) external {
+    function adjustGivingRate(uint _percentInterest) external {
         require(checkUserExistence(msg.sender), "You must be a depositor to adjust your give percentage.");
         userStructs[msg.sender].givePercent = _percentInterest;
+
+        emit AdjustGivingRate(msg.sender, _percentInterest);
     }
 
     // Read a user balance from the contract
@@ -124,12 +140,16 @@ contract Lend {
     function updateGiveAddress(address payable _giveAddress) public {
         require(msg.sender == admin, "You do not have permission to update the giving address.");
         giveAddress = _giveAddress;
+
+        emit AddressUpdated(msg.sender, _giveAddress);
     }
 
     // Transfer admin privileges
     function updateAdmin(address _newAdmin) external {
         require(msg.sender == admin, "Only the current admin can elect a new admin.");
         admin = _newAdmin;
+
+        emit AdminUpdated(msg.sender, admin);
     }
 
 }
